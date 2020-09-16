a8df127b0342ea5ba67e7fb8e175417e84d5e951
amo25 <amo25@vt.edu>
2020-09-15 21:50:52 -0400

    Working picker (Tested on iOS)

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Picker} from '@react-native-community/picker'
+
 
 export default function App() {
+  const [selectedValue, setSelectedValue] = React.useState("zero");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="5.0" value="zero" />
+        <Picker.Item label="5.1" value="one" />
+        <Picker.Item label="5.2" value="two" />
+      </Picker>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -19,3 +31,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
